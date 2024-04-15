b6d1ecc15dc0d5840495568809646b1b67c6ece2
Brandes98 <86222257+Brandes98@users.noreply.github.com>
2024-04-14 20:28:58 -0600

    Add files via upload
    si hay errores con pymongo ejecutar lo siguiente:
    docker compose down -v
    pip install pymongo
    poetry add pymongo
    docker compose build
    docker compose up

---
+++ P1BasesII-Ian2/P1BasesII-Ian/app.py
@@ -0,0 +1,51 @@
+import json
+import os
+from pymongo import MongoClient
+from bson import ObjectId
+from app_service import AppService
+from db import Database
+from flask import Flask, request, jsonify
+import redis
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+mongo_username = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+mongo_uri = f"mongodb://{mongo_username}:{mongo_password}@db:27017/"
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+
+db = Database(database=DB_NAME, host=DB_HOST, user=DB_USER, password=DB_PASSWORD, port=DB_PORT, uri=mongo_uri)
+
+app = Flask(__name__)
+mongodb_client = MongoClient(mongo_uri)
+appService = AppService(db)
+
+@app.route("/")
+def home():
+    return "App Works!!!"
+
+# Autenticación y Autorización
+@app.route("/auth/register", methods=["POST"])
+def insert_user():
+    user_data = request.get_json()
+    appService.insert_user(user_data)
+    return user_data
+
+@app.route("/auth/login", methods=["POST"])
+def login_user():
+    user_data = request.get_json()
+    token = appService.login_user(user_data)
+    return token
+
+# Usuarios
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = appService.get_users()
+    return jsonify(users)
